a45aca48b11a70309295931cf00ee1729da96dd3
Aleksey Petrenko <alekseypetrenko@users.noreply.github.com>
2018-03-27 23:07:08 +0300

    spread, default, rest

---
+++ dist/spread.js
@@ -0,0 +1,35 @@
+// let staticLanguages = ["C", "Java", "C++"];
+// let dynamicLanguages = ["JS", "Ruby", "PHP"];
+
+// если подставлять так, то выведутся массивы, а не их елементы
+// let otherLanguages = [staticLanguages, "Python", dynamicLanguages, "C#"];
+// console.log(otherLanguages);
+
+// Добавление ... решает проблему, babel конвертирует это в оператор concat на пусстом массиве в котором все массивы соединяются по порядку
+// var otherLanguages = [].concat(staticLanguages, ["Python"], dynamicLanguages, ["C#"]);
+// let otherLanguages = [...staticLanguages, "Python", ...dynamicLanguages, "C#"];
+// console.log(otherLanguages);
+
+// function sum (a , b, c) {
+//     console.log(a + b + c);
+// }
+
+// let arr = [1,2,3];
+// sum(...arr); 
+
+
+///////////////////// REST
+// function a(...b) {
+//     console.log(b);
+// }
+
+// a(1,2,34);
+
+// function sum(...args) {
+//     return args.reduce(
+//         (res, item) => res + item
+//     );
+// }
+
+// console.log(sum(2,3,4,5));
+"use strict";
